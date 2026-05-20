238de6ca6fe530c12d5c0cf6f16376e236e1f899
Blackspace2 <zezhong_xie@163.com>
2026-05-20 19:07:24 +0800

    feat: 汇总地面辅助监控与缺陷损失
    pcdet/models/detectors/second_net.py
    - 调整: 在 forward 中保留 batch_dict 作为训练期监控来源
    - 新增: 汇总 ground_guided_tb_dict 与 ground_context_tb_dict
    - 新增: 叠加 map_to_bev_module 的地面缺陷辅助损失

--- pcdet/models/detectors/second_net.py
+++ pcdet/models/detectors/second_net.py
@@ -5,10 +5,13 @@ class SECONDNet(Detector3DTemplate):
     def __init__(self, model_cfg, num_class, dataset):
         super().__init__(model_cfg=model_cfg, num_class=num_class, dataset=dataset)
         self.module_list = self.build_networks()
+        self.forward_ret_dict = {}
 
     def forward(self, batch_dict):
+        batch_dict['global_step'] = int(self.global_step.item())
         for cur_module in self.module_list:
             batch_dict = cur_module(batch_dict)
+        self.forward_ret_dict = batch_dict
 
         if self.training:
             loss, tb_dict, disp_dict = self.get_training_loss()
@@ -29,6 +32,15 @@ class SECONDNet(Detector3DTemplate):
             'loss_rpn': loss_rpn.item(),
             **tb_dict
         }
-
+        tb_dict.update(self.forward_ret_dict.get('ground_guided_tb_dict', {}))
+        tb_dict.update(self.forward_ret_dict.get('ground_context_tb_dict', {}))
         loss = loss_rpn
+
+        if hasattr(self, 'map_to_bev_module') and hasattr(self.map_to_bev_module, 'get_loss'):
+            loss_ground_defect, ground_defect_tb = self.map_to_bev_module.get_loss()
+            if loss_ground_defect is not None:
+                loss = loss + loss_ground_defect
+                tb_dict.update(ground_defect_tb)
+                tb_dict['loss_total'] = loss.item()
+
         return loss, tb_dict, disp_dict
